378966af4996b3bd5cf924d078ca6f1258fbdc4a
Jimmy Dang <jimmy.t.dang@gmail.com>
2020-01-14 10:26:51 -0500

    first commit

---
+++ fe-kitchen/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './components/Navbar'
+import SearchAppBar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+    <SearchAppBar color='secondary'></SearchAppBar>
+    </div>
+  );
+}
+
+export default App;
