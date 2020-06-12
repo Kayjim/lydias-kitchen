4951b858748238337e1c409a242e0aefd38654d5
chris patrick <lovedknightt@yahoo.com>
2020-06-12 12:59:40 -0400

    cors config

--- middleware/cors-config.js
+++ middleware/cors-config.js
@@ -1,7 +1,14 @@
 const cors = require('cors');
 
 var corsOptions = {
-    origin: ['https://lydias-kitchen.herokuapp.com', 'http://localhost:3000', 'http://localhost:4000', 'http://localhost:3004'],
+    origin: [
+        "http://www.lydiaskitchen.net",
+        "https://lydias-kitchen.herokuapp.com",
+        "lydias-kitchen.herokuapp.com",
+        "http://localhost:3000",
+        "http://localhost:4000",
+        "http://localhost:3004"
+      ],
     optionsSuccessStatus: 200
 };
 
